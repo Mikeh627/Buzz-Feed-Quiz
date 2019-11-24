bc654dd80253c1d52f69bc9c196f097733fa78c3
Michael Householder <householdcodermike@gmail.com>
2019-11-24 13:56:14 -0500

    Update qiuizCode.py

--- qiuizCode.py
+++ qiuizCode.py
@@ -1,47 +1,158 @@
-quiz_end = False
+print("Welcome! To the Spongebob Squarepants quiz!"), '\n'
+print("Here you will be asked a series of questions regarding food, activities"
+      ", etc. to see who you resemble most from Spongebob!"), '\n'
 
-questions = 1
+
+def invalid_input():
+    print("Invalid input, please make sure there are no grammatical errors or "
+          "incorrect capitals. :)")         # this is a function to no accept an invalid input
+
+
+quiz_end = False    # this sets the quiz up to run until the end where we will make this a true statement and print who you got
+
+questions = 1       # creating this variable with a int() will allow us to make the questions continue once the user inputs a str()
 
 patrick = 0
 sandy = 0
-mrKrabs = 0
+mrkrabs = 0         # these variables allow the program to know throughout the quiz which person has the hightest number which you get +1 for answering specific way throughout the quiz
 plankton = 0
 spongebob = 0
 
 while not quiz_end:
-    if questions == 1:
-        print("which of these foods would you choose?")
-        input(print("Money, Chum, IceCream, or KrabbyPatties")
+    if questions == 1:          #this is the start of our while loop, the questions will be in this until satisfactory amount of questions are prompted to the user
+        print()
+        print("Which of these foods would you choose?"),
+        print(input("Money, Chum, Ice Cream, or Krabby Patties?")),
 
-            if input == Money or money
-                (mrKrabs + 1)
+        if input == "Money" or "money":
+            mrkrabs + int(1)
 
-            else input == Chum or chum
-                (plankton + 1)
+        elif input == "Chum" or "chum":
+            plankton + int(1)                               #these input score the user into different variables as mentioned earlier
 
-            if input == Icecream or icecream
-                (patrick + 1)
+        elif input == "Ice Cream" or "ice cream" or "Ice Cream":
+            patrick + int(1)
 
-            else input == Krabbypatties or krabbypatties or KrabbyPatties
-                (spongebob + 1)
-                (sandy + 1)
+        elif input == "Krabby patties" or "Krabby Patties":
+            spongebob + int(1)
+            sandy + int(1)
 
+        else:
+            print("this selection is invalid, please correct any spelling "
+                  "errors, or choose a valid response :)")
+            questions = 1
+
+    questions += 1          # this little guy allows us to go up after every question is answered
 
     if questions == 2:
-        print("What is your favorite color?")
-        input(print("Yellow, Pink, Green, or Brown?")
+        print()
+        print("Which of the following is your favorite pass time?"),
+        print(input("Money, cooking, science, Jellyfishing, or sleeping?"))
 
-            if input == Yellow or yellow
-                (spongebob + 1)
-                (sandy + 1)
+        if input == "Money" or "money":
+            mrkrabs + int(1)
 
-            else input == Pink or pink
-                (patrick + 1)
+        elif input == "science" or "Science":
+            plankton + int(1)
+            sandy + int(1)
 
-            if input == Green or green
-                (mrKrabs + 1)
+        elif input == "Sleeping" or "sleeping":
+            patrick + int(1)
 
-            else input == Brown or brown
-                (plankton + 1)
+        elif input == "Cooking" or "cooking":
+            spongebob + int(1)
+
+        elif input == "Jelly Fishing" or "jellyfishing":
+            spongebob + int(1)
+            patrick + int(1)
+
+        else:
+            print("this selection is invalid, please correct any spelling "
+                  "errors, or choose a valid response :)")
+            questions = 2
+
+    questions += 1
+
+    if questions == 3:
+        print()
+        print("Which of the following is your favorite color?"),
+        print(input("Green, yellow, pink or blue?"))
+
+        if input == "Green " or "green":
+            mrkrabs + int(1)
+
+        elif input == "Yellow" or "yellow":
+            spongebob + int(1)
+            sandy + int(1)
+
+        elif input == "pink" or "Pink":
+            patrick + int(1)
+
+        elif input == "blue" or "Blue":
+            plankton + int(1)
+
+        else:
+            print("this selection is invalid, please correct any spelling "
+                  "errors, or choose a valid response :)")
+            questions = 3
+
+    questions += 1
+
+    if questions == 4:
+        print()
+        print("How would your friends describe you?"),
+        print(input("Stupid, Funny, Stingy, Smart, or Creative?"))
+
+        if input == "Stingy" or "stingy":
+            mrkrabs + int(1)
+            plankton + int(1)
+
+        elif input == "Smart" or "Smart":
+            plankton + int(1)
+            sandy + int(1)
+
+        elif input == "Stupid" or "stupid":
+            patrick + int(1)
+            spongebob + int(1)
+
+        elif input == "Funny" or "Funny":
+            patrick + int(1)
+
+        elif input == "creative" or "Creative":
+            sandy + int(1)
+
+        else:
+            print("this selection is invalid, please correct any spelling "
+                  "errors, or choose a valid response :)")
+            questions = 4
+
+    questions += 1
+
+    if questions == 5:
+        print()
+        print(""),
+        print(input(""))
+
+        if input == "Money" or "money":
+            mrkrabs + int(1)
+
+        elif input == "science" or "Science":
+            plankton + int(1)
+            sandy + int(1)
+
+        elif input == "Sleeping" or "sleeping":
+            patrick + int(1)
+
+        elif input == "Cooking" or "cooking":
+            spongebob + int(1)
+
+        elif input == "Jelly Fishing" or "jellyfishing":
+            spongebob + int(1)
+            patrick + int(1)
+
+        else:
+            print("this selection is invalid, please correct any spelling "
+                  "errors, or choose a valid response :)")
+            questions = 5
 
     questions += 1
